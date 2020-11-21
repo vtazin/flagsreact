b03cb7c623e49c8e5dd820ba7bc57bb1c395b5e4
Victor <vtazin@gmail.com>
2020-11-21 14:10:20 +0300

    Filepath fixed

--- scripts/server.js
+++ scripts/server.js
@@ -3,10 +3,10 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "build")));
+app.use(express.static(path.resolve(__dirname, "..", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
 });
 
 app.listen(port);
